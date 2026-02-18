9402e32fd18aad2c37600f9c97efc5c1fa880910
ALPilapil <alpilapil@ucdavis.edu>
2026-02-17 20:51:48 -0800

    added annotations for movement prompting, sttill need to do jaw clenches

--- processing/add_annotations.py
+++ processing/add_annotations.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 import os
+import mne
+import pandas as pd
+from datetime import datetime, date
+import math
+
 
 def get_filenames(folder_name):
     '''
@@ -20,13 +25,13 @@ def get_pairs_util(psychopy_files, eeg_files):
     # since we only do each person once a day max, they will always be unique
     pairs = []
 
-    # go through list, find according file
-    eeg_lookup = {os.path.splitext(f)[0]: f for f in eeg_files}
+    eeg_edf_lookup = {os.path.splitext(f)[0]: f for f in eeg_files if f.endswith('.edf')}
+    eeg_csv_lookup = {os.path.splitext(f)[0]: f for f in eeg_files if f.endswith('.csv')}
 
     for file in psychopy_files:
-        base = os.path.splitext(file)[0]
-        if base in eeg_lookup:
-            pairs.append((file, eeg_lookup[base]))
+        base = os.path.splitext(file)[0].removesuffix('pp')
+        if base in eeg_edf_lookup and base in eeg_csv_lookup:
+            pairs.append((file, eeg_edf_lookup[base], eeg_csv_lookup[base]))
 
     return pairs
 
@@ -45,14 +50,77 @@ def get_pairs(psychopy_folder, eeg_folder):
 
     # make pairs according to naming conventions
     pairs = get_pairs_util(psychopy_files, eeg_files)
-    print(pairs)
+    return pairs
 
-def annotate(pairs):
+def get_time(lines):
+    '''
+    input: a list of meta data lines
+    output: a time
+    '''
+    for line in lines:
+        if line.startswith('# Time ='):
+            time_str = line.split(', ')[1].split(',')[0].strip()
+            break
+    
+    time = datetime.strptime(time_str, '%H:%M:%S.%f').time()
+
+    return time
+
+def get_psycho_time(df):
+    '''
+    input: path to a psychopy time
+    output: a time
+    '''
+    start_col = df['expStart']
+    start_time = start_col[0]
+    start = datetime.strptime(start_time, '%Y-%m-%d %Hh%M.%S.%f %z').time()
+    return start
+
+def annotate(pairs, output_folder, eeg_folder, psychopy_folder):
     '''
     input: list of pairs of files
     output: edf files that now have event event codes
     '''
 
+    for pair in pairs:
+        eeg_file = pair[1]
+        eeg_info = pair[2]
+        psycho_file = pair[0]
+        # load in the raw
+        raw = mne.io.read_raw_edf(f'{eeg_folder}/{eeg_file}', preload=False)
+        # get the start time for the eeg data and the psychopy data
+        with open(f'{eeg_folder}/{eeg_info}') as f:
+            lines = f.readlines()
+        
+        # establish eeg time
+        eeg_time = get_time(lines)
+        # establish psychopy time
+        df = pd.read_csv(f'{psychopy_folder}/{psycho_file}')
+        psychopy_time = get_psycho_time(df)
+        # get stimulus onset times
+        movement_times = df['go_txt.started'].tolist()
+        clean_move_times = [x for x in movement_times if not math.isnan(x)]
+
+        # get difference of times, eeg will always start sooner
+        diff = datetime.combine(date.min, psychopy_time) - datetime.combine(date.min, eeg_time)
+        seconds_diff = diff.total_seconds()
+
+        # add seconds to all elements of psychopy_time
+        move_onsets = [x + seconds_diff for x in clean_move_times]
+        move_durations = [4.0] * len(move_onsets)
+        move_desc = ['move'] * len(move_onsets)
+
+        # replicate this with jaw clenches at concatenate them
+        onsets = move_onsets
+        durations = move_durations
+        descriptions = move_desc
+
+        # set annotations
+        annotation = mne.Annotations(onset=onsets, duration=durations, description=descriptions)
+        raw.set_annotations(annotation)
+
+        # save
+        
 
 
 
@@ -68,9 +136,10 @@ def main():
 
     # for each eeg + psychopy pair
     pairs = get_pairs(psychopy_folder, eeg_folder)
+    print(pairs)
 
     # get the time of each MI cue
-    annotate(pairs)
+    annotate(pairs=pairs, output_folder=annotated_eeg_folder, eeg_folder=eeg_folder, psychopy_folder=psychopy_folder)
 
     # add an event for each of these cues in the EEG file
 
