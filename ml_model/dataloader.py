3f5a556e5b13c7cc570490d0f1fe4d17627fb355
ALPilapil <alpilapil@ucdavis.edu>
2026-03-23 16:49:59 -0700

    dataloader setup, will be necessary for deep learning

---
+++ ml_model/dataloader.py
@@ -0,0 +1,34 @@
+from torch.utils.data import Dataset
+import torch
+import numpy as np
+import mne
+
+
+class EEGDataset(Dataset):
+    def __init__(self):
+        '''
+            
+        '''
+
+
+    def __len__(self):
+        '''
+        '''
+
+
+    def __getitem__(self, idx):
+        '''
+        '''
+
+
+def main():
+    '''
+    test it
+    '''
+    data = np.load('dataset.npz')
+    X, y = data['X'], data['y']
+
+    dataset = EEGDataset()
+
+if __name__ == "__main__":
+    main()
