185da26e0f442d1716a7ee8dfb7c949a579bc1f1
ALPilapil <alpilapil@ucdavis.edu>
2026-03-23 16:49:45 -0700

    minor

---
+++ main.py
@@ -0,0 +1,21 @@
+
+def main():
+    '''
+    continuously run the lidar, gathering data
+    continuously get eeg data
+    builds walls and map as you go
+
+    if (eye tracker == point on screen and classifier == go)
+      short circut evaluation will make sure classifier only runs 
+      if eye tracker already is valid
+      use the eye tracker data to pathfind 
+      return the output of this pathfinding
+    '''
+    
+
+
+
+    return
+
+if __name__ == "__main__":
+    main()
