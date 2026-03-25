fdcc32bd68c594208b18fa3bff05ba120e3e3219
ALPilapil <alpilapil@ucdavis.edu>
2026-03-24 20:50:45 -0700

    minor note on future implementation

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ def main():
     continuously run the lidar, gathering data
     continuously get eeg data
     builds walls and map as you go
+    should make a separate classifier that does only idle vs jaw clench that always runs
+    regardless of eye tracker input so that emergency stop always works but with 
+    an easier classification task
 
     if (eye tracker == point on screen and classifier == go)
       short circut evaluation will make sure classifier only runs 
